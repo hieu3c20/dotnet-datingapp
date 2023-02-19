0af7df8a95aa563400c8168aa46584989f2e18c6
hieu3c20 <hovanhieu2012sch@gmail.com>
2023-02-19 11:42:12 +0700

    using conditional to show remove form login logout

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
   model: any = {};
+  loggedIn: boolean = false;
 
-  constructor() {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {}
 
   login() {
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe(
+      (response) => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  logout() {
+    this.loggedIn = false;
   }
 }
