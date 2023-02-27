2b22f784b1fe62ab6d2c44d40f332e2d97a94a88
hieu3c20 <hovanhieu2012sch@gmail.com>
2023-02-27 15:14:52 +0700

    8.4 adding a member service

---
+++ client/src/app/_services/member.service.ts
@@ -0,0 +1,31 @@
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Member } from '../_models/member';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token,
+  }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'user', httpOptions);
+  }
+
+  getMember(username) {
+    return this.http.get<Member>(
+      this.baseUrl + 'users/' + username,
+      httpOptions
+    );
+  }
+}
