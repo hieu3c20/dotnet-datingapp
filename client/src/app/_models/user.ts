0d1290b7eac8a6e1526a1b44d49986ec74e780d3
hieu3c20 <hovanhieu2012sch@gmail.com>
2023-02-27 14:43:36 +0700

    9.2 using type script

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -2,3 +2,27 @@ export interface User {
   username: string;
   token: string;
 }
+
+let data: number | string = 42;
+data = '10';
+
+interface Car {
+  color: string;
+  model: string;
+  topSpeed?: number;
+}
+
+const car1 = {
+  color: 'blue',
+  model: 'BMW',
+};
+
+const car2 = {
+  color: 'red',
+  model: 'Mercedes',
+  topSpeed: 100,
+};
+
+const multiply = (x: number, y: number): void => {
+  x * y;
+};
