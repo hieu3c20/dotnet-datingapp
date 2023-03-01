ba00549ee728f74d145f92e4a86970f43cdba591
hieu3c20 <hovanhieu2012sch@gmail.com>
2023-03-01 16:03:33 +0700

    9.9 using an interceptor to send the token

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -4,12 +4,6 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Member } from '../_models/member';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
-  }),
-};
-
 @Injectable({
   providedIn: 'root',
 })
@@ -19,13 +13,12 @@ export class MemberService {
   constructor(private http: HttpClient) {}
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username) {
     return this.http.get<Member>(
-      this.baseUrl + 'users/' + username,
-      httpOptions
+      this.baseUrl + 'users/' + username
     );
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { JwtInterceptor } from './_intercepters/jwt.interceptor';
 import { ErrorInterceptor } from './_intercepters/error.interceptor';
 import { SharedModule } from './_modules/shared.module';
 import { NgModule } from '@angular/core';
@@ -32,7 +33,8 @@ import { MemberCardComponent } from './members/member-card/member-card.component
     SharedModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
   ],
   bootstrap: [AppComponent],
 })
