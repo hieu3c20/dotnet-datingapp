1049da1732e8a66b0d1cd2a5adc2b9ebb5a0f5d1
hieu3c20 <hovanhieu2012sch@gmail.com>
2023-03-13 15:11:45 +0700

    11.12 adding the main photo to the nav bar

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -7,8 +7,10 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string url { get; set; }
-        public bool isMain { get; set; }
-        public string PublicId { get; set; }
+    public string Url { get; internal set; }
+    public bool isMain { get; set; }
+    public bool IsMain { get; internal set; }
+    public string PublicId { get; set; }
         public AppUser AppUser { get; set; }
         public int AppUserId { get; set; }
     }
