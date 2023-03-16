4688cb8b7fdbabf8294c16dedeeee337d694cab6
hieu3c20 <hovanhieu2012sch@gmail.com>
2023-03-16 11:23:43 +0700

    13.2 adding a paged list class

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -8,7 +8,6 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        public string Username { get; internal set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public DateTime DateOfBirth { get; set; }
