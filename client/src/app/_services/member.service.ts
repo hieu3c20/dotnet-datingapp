5a4876bf8e882f3f655cba4707967eb8c1fc3e8f
hieu3c20 <hovanhieu2012sch@gmail.com>
2023-03-18 16:29:07 +0700

    13.17 restoring caching for member detailed

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -44,8 +44,18 @@ export class MemberService {
   }
 
   getMember(username: string) {
-    const member = this.members.find((x) => x.username === username);
-    if (member !== undefined) return of(member);
+    // const member = this.members.find((x) => x.username === username);
+    // if (member !== undefined) return of(member);
+    const member = [...this.memberCache.values()]
+      .reduce((arr, elem) => 
+        arr.concat(elem.result), []
+      )
+      .find((member: Member) => member.username === username);
+
+    if (member) { 
+      return of(member);
+    }
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
