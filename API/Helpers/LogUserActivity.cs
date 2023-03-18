c9fc58e9ea6bb30213854a73185b67b4690f82f6
hieu3c20 <hovanhieu2012sch@gmail.com>
2023-03-18 15:00:10 +0700

    13.13 making the last active action filter more op

--- API/Helpers/LogUserActivity.cs
+++ API/Helpers/LogUserActivity.cs
@@ -6,20 +6,25 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace API.Helpers
 {
-  public class LogUserActivity : IAsyncActionFilter
-  {
-    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    public class LogUserActivity : IAsyncActionFilter
     {
-      var resultContext = await next();
+        public async Task OnActionExecutionAsync(
+            ActionExecutingContext context,
+            ActionExecutionDelegate next
+        )
+        {
+            var resultContext = await next();
 
-      if (resultContext.HttpContext.User.Identity.IsAuthenticated)
-      {
-        var username = resultContext.HttpContext.User.GetUsername();
-        var repo = resultContext.HttpContext.RequestServices.GetService(typeof(IUserRepository)) as IUserRepository;
-        var user = await repo.GetUserByUserNameAsync(username);
-        user.LastActive = DateTime.Now;
-        await repo.SaveAllAsync();
-      }
+            if (resultContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                var userId = resultContext.HttpContext.User.GetUserId();
+                var repo =
+                    resultContext.HttpContext.RequestServices.GetService(typeof(IUserRepository))
+                    as IUserRepository;
+                var user = await repo.GetUserByIdAsync(userId);
+                user.LastActive = DateTime.Now;
+                await repo.SaveAllAsync();
+            }
+        }
     }
-  }
 }
