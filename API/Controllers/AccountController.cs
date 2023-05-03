c852d17b1813b917e7e5695f295defe5a0ed38d5
hieu3c20 <hovanhieu2012sch@gmail.com>
2023-05-03 21:30:33 +0700

    16.2 Setting up the entities

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -35,14 +35,10 @@ namespace API.Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-
             user.UserName = registerDto.Username;
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
 
             _context.Users.Add(user);
-            await _context.SaveChangesAsync(); 
+            await _context.SaveChangesAsync();
             return new UserDto
             {
                 Username = user.UserName,
@@ -62,15 +58,6 @@ namespace API.Controllers
             if (user == null)
                 return Unauthorized("invalid username");
 
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i])
-                    return Unauthorized("invalid password");
-            }
             return new UserDto
             {
                 Username = user.UserName,
